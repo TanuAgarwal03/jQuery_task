f6e173baa4e5b2dde877abd9e1d7f24000cadb75
Tanu <tanuagarwal.0305@gmail.com>
2024-03-08 12:57:06 +0530

    code added

--- script.js
+++ script.js
@@ -49,14 +49,6 @@
 //       dropdown.appendChild(optionElement);
 //     });
 //   }
-let selectedIndex;
-let selectedIndex2;
-let selectedIndex3;
-
-const mySubmitFunction =(e) => {
-    e.preventDefault();
-    return false;
-};
 
 // $(document).ready(function() {
 //   // Event handler for dropdown1 change event
@@ -79,6 +71,14 @@ const mySubmitFunction =(e) => {
 //     }
 //   });
 // });
+let selectedIndex;
+let selectedIndex2;
+let selectedIndex3;
+
+const mySubmitFunction =(e) => {
+    e.preventDefault();
+    return false;
+};
 
 const disabledBtnForm = (inputField, btnId) => {
     $(btnId).prop("disabled" , true);  //initially disable the submit button
@@ -147,32 +147,228 @@ const getIndexOfSelectedHeading2 = () => {
       selectedIndex3 = $(this).prop("selectedIndex");
     });
   };
+  const sortableFunction = () => {
+    $(function () {
+    $("main").sortable({
+    update: function (event, ui) { //this event is triggered when sorting operation is completed
+    saveData();   //this function is called to save the updated order of elements 
+    },
+    cursor: "move",
+    });
+    $("aside").sortable({
+    update: function (event, ui) {
+    saveData();
+    },
+    cursor: "move",
+    connectWith: "aside",
+    });
+    $(".mx-5").sortable({
+    connectWith: "aside > div",
+    items: "> form",
+    cursor: "move",
+    update: function (event, ui) {
+    saveData();
+    },
+    });
+    });
+   };
 
 
-  const inputTypesData = [
-    {
-      type: "text",
-      attrs: ["name", "placeholder", "maxlength", "label", "required"],
-    },
-    {
-      type: "textarea",
-      attrs: ["name", "placeholder", "maxlength", "label", "required"],
-    },
+  const inputTypesData = [     //array containing objects with input types and their attributes
+    { type: "text", attrs: ["name", "placeholder", "maxlength", "label", "required"] },
+    { type: "textarea", attrs: ["name", "placeholder", "maxlength", "label", "required"] },
     { type: "number", attrs: ["name", "min", "max", "label"] },
     { type: "email", attrs: ["name", "placeholder", "label"] },
     { type: "password", attrs: ["name", "minlength", "maxlength", "label"] },
     { type: "date", attrs: ["name", "min", "max"] },
     { type: "button", attrs: ["name", "value"] },
   ];
-  const OnlyInputType = () => {
+  const OnlyInputType = () => { //function to extract only the input types from above function
     const inputType = inputTypesData.map((ele) => ele.type);
     return inputType;
   };
 
+  function updateSelectOptions(array, selectid) { //function to update the options of dropdown based on array of values 
+    if (!array) {
+      return;
+    }
+  
+    var select = $(selectid);
+    select.empty();
+    var defaultOption = $("<option>", {  //create a default option with some text
+      value: null,
+      text: "Select any of the heading...",
+    });
+    select.append(defaultOption);  //append the default option to the dropdown 
+    for (var i = 0; i < array.length; i++) { 
+      var option = $("<option>", { //Create an option element for each value in the array
+        value: array[i], 
+        text: array[i] 
+    });
+      select.append(option);
+    }
+  }
+
+  $(document).ready(function () {
+    loadData();  //load data from localstorage or database 
+    sortableFunction();
+    getIndexOfSelectedHeading();
+    getIndexOfSelectedHeading2();
+    getIndexOfSelectedHeading3();
+    disabledBtnForm("#input1", "#btn1");
+  
+    $("main").on("click", ".delete-section", function () {
+      $(this).closest("section").remove();  //removes the closest section 
+      saveData();  //save the changes made to the data 
+    });
+    $("main").on("click", ".delete-subheading", function () {
+      $(this).closest(".mx-5").remove(); //removes the subheading section from DOM
+      saveData();
+    });
+       
+    $("main").on("click", ".delete-input", function () {
+      var inputContainer = $(this).closest("form"); //removes the form section from DOM
+      inputContainer.remove();
+      saveData();
+    });
+    updateSelectOptions(OnlyInputType(), "#selctinputtype");
+    
+    $("#headingModalbtn").click(function () {
+        $("#input1").val("");
+    });
+
+    $("#SubheadingModalbtn").click(function () {
+        $("#input2").val("");
+        populateHeadingSelect();
+    });
+
+    $("#form1").click(function () {
+        $("#selectsubheading").empty();
+        const defaultOption = `<option> select subheading </option>`;
+        $("#selectsubheading").append(defaultOption);
+        $("#attributeForm").remove();
+        $("#selctinputtype").val("Select any of the input...");
+        populateHeadingSelectinput2();
+    });
+
+    $("#btn1").click(function () {
+    var heading = $("#input1").val();
+    if (heading == "") {
+    alert("enter heading");
+    return;
+    }
+    appendHeading(heading);
+    saveData();
+    $("#input1").val("");
+    sortableFunction();
+    });
+
+    $("#btn2").click(function () {
+    var heading = $("#dynamicSelect").val();
+    var subheading = $("#input2").val();
+    if (heading === "" || subheading === "" || heading == "Select Heading") {
+        return;
+    } else {
+        appendSubheading(heading, subheading);
+        saveData();
+    }
+    $("#input2").val("");  //clear the value of element with id input2 
+    $("#exampleModal2").removeClass("show");
+    $("#exampleModal2").modal("hide");
+    sortableFunction();
+    });
+       
+    var optionsData = [];
+
+    $("#btn3").click(() => {
+    const input1 = $("#selectheading").val();
+    const input2 = $("#selectsubheading").val();
+    const input3 = $("#selctinputtype").val();
+    console.log(input1,input2,input3)
+    var errors = []; //array to store the arrays is any of these conditions gets true 
+       
+    if (input1 == "Select heading") {
+        errors.push("Please select a valid heading for Heading.");
+    }
+        
+    if (input2 == "select subheading" || input2 == null) {
+        errors.push("Please select a valid subheading for Sub Heading.");
+    }
+        
+    if (input3 == "Select any of the input...") {
+        errors.push("Please select a valid heading for Input Type.");
+    }
+        
+    if (errors.length > 0) {
+        $("#error").text(errors.join("\n"))
+        errors.splice(0,errors.length)
+        return;
+    }
+    
+       
+   })
+  })
+
+// $(document).ready(function() {
+//     $('#input1').on('input', function() {
+//         var inputValues = $(this).val().trim().split(','); // Split input values by comma
+        
+//         // Clear previous options in the dropdown
+//         $('#selectheading').empty();
+        
+//         // Add new options to the dropdown
+//         $.each(inputValues, function(index, value) {
+//             // Trim leading/trailing whitespace from each value
+//             value = value.trim();
+//             if (value !== '') {
+//                 // Append an option element to the dropdown
+//                 $('#selectheading').append($('<option>', {
+//                     value: value,
+//                     text: value
+//                 }));
+//             }
+//         });
+//     });
+// });
 
 
+// on refreshing the page , the content in the field remain intact
 
+// $(document).ready(function() {
+//     // Load data from localStorage if available
+//     var savedValues = localStorage.getItem('inputValues');
+//     if (savedValues) {
+//         $('#input1').val(savedValues);
+//         updateDropdown(savedValues);
+//     }
+    
+//     $('#input1').on('input', function() {
+//         var inputValues = $(this).val().trim();
+//         updateDropdown(inputValues);
+        
+//         // Save input values to localStorage
+//         localStorage.setItem('inputValues', inputValues);
+//     });
+    
+//     function updateDropdown(inputValues) {
+//         var inputArray = inputValues.split(',').map(function(value) {
+//             return value.trim();
+//         }).filter(function(value) {
+//             return value !== '';
+//         });
 
+//         // Clear previous options in the dropdown
+//         $('#selectheading').empty();
+
+//         // Add new options to the dropdown
+//         $.each(inputArray, function(index, value) {
+//             $('#selectheading').append($('<option>', {
+//                 value: value,
+//                 text: value
+//             }));
+//         });
+//     }
+// });
 
 
 
