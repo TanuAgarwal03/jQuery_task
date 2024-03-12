6152fb51d7b4d57d043235c9c9b017ef5d1a4f73
Tanu <tanuagarwal.0305@gmail.com>
2024-03-12 13:57:36 +0530

    commit changes

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ const mySubmitFunction =(e) => {
 const disabledBtnForm = (inputField, btnId) => {
     $(btnId).prop("disabled" , true);  //initially disable the submit button
     $(inputField).on("input" , function(){
-        const textInput = $(this).val();
+        const textInput = $(this).val().trim();
         if(textInput.length <= 0){      //no heading text is entered
             $(btnId).prop("disabled" , true);
         }
@@ -147,40 +147,48 @@ const getIndexOfSelectedHeading2 = () => {
       selectedIndex3 = $(this).prop("selectedIndex");
     });
   };
-  const sortableFunction = () => {
-    $(function () {
-    $("main").sortable({
-    update: function (event, ui) { //this event is triggered when sorting operation is completed
-    saveData();   //this function is called to save the updated order of elements 
-    },
-    cursor: "move",
-    });
-    $("aside").sortable({
-    update: function (event, ui) {
-    saveData();
-    },
-    cursor: "move",
-    connectWith: "aside",
-    });
-    $(".mx-5").sortable({
-    connectWith: "aside > div",
-    items: "> form",
-    cursor: "move",
-    update: function (event, ui) {
-    saveData();
-    },
-    });
-    });
-   };
 
+   const sortableFunction = () => {
+    $(function () {
+      $("main").sortable({
+        update: function (event, ui) { //this event is triggered when sorting operation is completed
+          saveData();   //this function is called to save the updated order of elements 
+        },
+        cursor: "move",
+      });
+      $("aside").sortable({
+        update: function (event, ui) {
+          saveData();
+        },
+        cursor: "move",
+        connectWith: "aside",
+      });
+      $(".mx-5").sortable({
+        connectWith: "aside > div",
+        items: "> form",
+        cursor: "move",
+        update: function (event, ui) {
+          saveData();
+        },
+      });
+    });
+  };
 
   const inputTypesData = [     //array containing objects with input types and their attributes
     { type: "text", attrs: ["name", "placeholder", "maxlength", "label", "required"] },
     { type: "textarea", attrs: ["name", "placeholder", "maxlength", "label", "required"] },
     { type: "number", attrs: ["name", "min", "max", "label"] },
     { type: "email", attrs: ["name", "placeholder", "label"] },
+    { type: "checkbox", attrs: ["name", "checked", "label"] },
+    { type: "radio", attrs: ["name", "value", "checked", "label"] },
     { type: "password", attrs: ["name", "minlength", "maxlength", "label"] },
     { type: "date", attrs: ["name", "min", "max"] },
+    { type: "file", attrs: ["name", "accept"] },
+    { type: "color", attrs: ["name", "value"] },
+    { type: "range", attrs: ["name", "min", "max", "step", "value", "label"] },
+    { type: "hidden", attrs: ["name", "value"] },
+    { type: "submit", attrs: ["name", "value"] },
+    { type: "reset", attrs: ["name", "value"] },
     { type: "button", attrs: ["name", "value"] },
   ];
   const OnlyInputType = () => { //function to extract only the input types from above function
@@ -197,7 +205,7 @@ const getIndexOfSelectedHeading2 = () => {
     select.empty();
     var defaultOption = $("<option>", {  //create a default option with some text
       value: null,
-      text: "Select any of the heading...",
+      text: "Select any input data type",
     });
     select.append(defaultOption);  //append the default option to the dropdown 
     for (var i = 0; i < array.length; i++) { 
@@ -233,7 +241,7 @@ const getIndexOfSelectedHeading2 = () => {
     });
     updateSelectOptions(OnlyInputType(), "#selctinputtype");
     
-    $("#headingModalbtn").click(function () {
+    $("#headingModal").click(function () {
         $("#input1").val("");
     });
 
@@ -301,13 +309,308 @@ const getIndexOfSelectedHeading2 = () => {
         
     if (errors.length > 0) {
         $("#error").text(errors.join("\n"))
-        errors.splice(0,errors.length)
+        errors.splice(0,errors.length) //it removes elements from starting index 0 upto the end of array errors
         return;
     }
-    
-       
-   })
-  })
+
+    var section = $("main > section").eq(selectedIndex2 - 1);
+    var targetDiv = section.find("aside > div.mx-5").eq(selectedIndex3 - 1);
+    const selectedDiv = targetDiv;
+    if(input3 === "textarea"){
+      var selectElement = $(
+        "<form class=' p-2 my-2 d-flex align-items-center mx-4'><textarea class='form-select'></textarea></form>"
+      );
+      selectedDiv.append(selectElement);
+      saveData();
+    }
+    else {
+      var id = $("#id").val();
+      var className = $("#class").val();
+      var value = $("#value").val();
+      var disabled = $("#disabled").prop("checked");
+      var readonly = $("#readonly").prop("checked");
+      var require = $("#require").prop("checked");
+      var placeholder = $("#placeholder").val();
+      var maxlength = $("#maxlength").val();
+      var minLength = $("#minLength").val();
+      var name = $("#inputName").val();
+      var newInput = $(
+        `<form class=' p-2 my-2 d-flex align-items-center mx-4'><input class='w-50 d-block mt-2'><button class='delete-input btn btn-info btn-sm'>Delete Input</button></form>`
+      );
+      switch (input3) {
+        case "text":
+          newInput.find("input").attr({
+            id: id,
+            type: "text",
+            class: className,
+            value: value,
+            disabled: disabled ? "disabled" : undefined,
+            readonly: readonly ? "readonly" : undefined,
+            required: require ? "required" : undefined,
+            placeholder: placeholder,
+            maxlength: maxlength,
+            minlength: minLength,
+          });
+          break;
+        case "number":
+          newInput.find("input").attr({
+            id: id,
+            type: "number",
+            class: className,
+            value: value,
+            disabled: disabled ? "disabled" : undefined,
+            readonly: readonly ? "readonly" : undefined,
+             required: require ? "required" : undefined,
+            placeholder: placeholder,
+            maxlength: maxlength,
+            minlength: minLength,
+          });
+          break;
+        case "checkbox":
+          newInput = $(
+            `<form class=' p-2 my-2 d-flex align-items-center mx-4'><button class='delete-input btn btn-info btn-sm'>Delete Input</button></form>`
+          );
+          var checkboxValues = value.split(",");
+          checkboxValues.forEach(function (checkboxValue) {
+            checkboxValue = checkboxValue.trim();
+            var checkboxElement = $("<input>", {
+              id: id + "_" + checkboxValue,
+              type: "checkbox",
+              class: className,
+              disabled: disabled ? "disabled" : undefined,
+              readonly: readonly ? "readonly" : undefined,
+              required: require ? "required" : undefined,
+            });
+            var labelElement = $("<label>", {
+              for: id + "_" + checkboxValue,
+              text: checkboxValue,
+            });
+            newInput.prepend(checkboxElement, labelElement);
+          });
+          break;
+        case "radio":
+          newInput = $(
+            `<form class=' p-2 my-2 d-flex align-items-center mx-4'><button class='delete-input btn btn-info btn-sm'>Delete Input</button></form>`
+          );
+          var radioValue = value.split(",");
+          radioValue.forEach(function (radioValues) {
+            radioValues = radioValues.trim();
+            var radioElement = $("<input>", {
+              id: id + "_" + radioValues,
+              type: "radio",
+              class: className,
+              disabled: disabled ? "disabled" : undefined,
+              readonly: readonly ? "readonly" : undefined,
+               required: require ? "required" : undefined,
+            });
+            var labelElement = $("<label>", {
+              for: id + "_" + radioValues,
+              text: radioValues,
+            });
+            newInput.prepend(radioElement, labelElement);
+          });
+          break;
+        case "submit":
+          newInput
+            .find("input")
+            .attr({
+              id: id,
+              type: "submit",
+              class: className,
+              value: value,
+              name: name,
+              disabled: disabled ? "disabled" : undefined,
+              readonly: readonly ? "readonly" : undefined,
+               required: require ? "required" : undefined,
+            });
+          break;
+        case "range":
+          console.log("here in submit", maxlength, minLength);
+          newInput
+            .find("input")
+            .attr({
+              type: "range",
+              id: id,
+              min: minLength,
+              max: maxlength,
+              value: value,
+              disabled: disabled ? "disabled" : undefined,
+              readonly: readonly ? "readonly" : undefined,
+               required: require ? "required" : undefined,
+            });
+          break;
+        default:
+          newInput
+            .find("input")
+            .attr({
+              id: id,
+              type: input3,
+              class: className,
+              value: value,
+              disabled: disabled ? "disabled" : undefined,
+              readonly: readonly ? "readonly" : undefined,
+               required: require ? "required" : undefined,
+              placeholder: placeholder,
+              maxlength: maxlength,
+              minlength: minLength,
+            });
+          break;
+      }
+
+      selectedDiv.append(newInput);
+    }
+    $("#selectheading").val("Select heading");
+    $("#selectsubheading").val("select subheading");
+    $("#selctinputtype").val("Select any input data type");
+    var takeinputdata = $("#takeinputdata");
+    appendForm(takeinputdata);
+    $("#exampleModal3").removeClass("show");
+    $("#exampleModal3").modal("hide");
+    saveData();
+  });
+
+  function populateHeadingSelect() {
+    var headings = $("h2");
+    var select = $("#dynamicSelect");
+    select.empty();
+    select.append("<option selected>" + "Select Heading" + "</option>");
+    headings.each(function () {
+      select.append("<option>" + $(this).text() + "</option>");
+    });
+  }
+  function populateHeadingSelectinput2() {
+    var headings = $("h2");
+    var select = $("#selectheading");
+    select.empty();
+    select.append("<option>" + "Select heading" + "</option>");
+    headings.each(function () {
+      select.append("<option>" + $(this).text() + "</option>");
+    });
+  }
+
+  function appendHeading(heading) {
+    $("main").append(
+      "<section class='bg-light w-50 my-2 p-2 position-relative'><button class='delete-section btn btn-danger position-absolute end-0 mx-2'>⨯</button><h2 class='heading'>" +
+        heading +
+        "</h2><aside class=' p-3'></aside></section>"
+    );
+  }
+
+  function appendSubheading(heading, subheading) {
+    getIndexOfSelectedHeading();
+    console.log(selectedIndex);
+    var section = $("main > section").eq(selectedIndex - 1);
+    if (section.length === 0) {
+      console.error("Section not found for heading: " + heading);
+      return;
+    }
+
+    var newSubheadingDiv = $(
+      "<div class='mx-5 bg-light w-50 my-2 p-2 position-relative'><h4 class='subheading'>" +
+        subheading +
+        "</h4><button class='delete-subheading btn btn-danger position-absolute end-0 top-0 mx-2'>⨯</button></div>"
+    );
+
+    section.find("aside").append(newSubheadingDiv);
+  }
+
+  function loadData() {
+    var contentData = localStorage.getItem("contentData");
+    if (contentData) {
+      $("main").html(contentData);
+    }
+  }
+});
+const populateheadingsub = () => {
+  var section = $("main > section").eq(selectedIndex2 - 1);
+  console.log(section, "section....");
+  const h4Elements = section.find("h4.subheading");
+
+  console.log(h4Elements, "h4elements....");
+  const h4TextArray = h4Elements
+    .map(function () {
+      return $(this).text();
+    })
+    .get();
+  console.log(h4TextArray);
+  var selectsubheading = $("#selectsubheading");
+
+  selectsubheading.empty();
+  selectsubheading.append("<option>" + "select subheading" + "</option>");
+  if (h4TextArray && h4TextArray.length) {
+    for (let i = 0; i < h4TextArray.length; i++) {
+      selectsubheading.append("<option>" + h4TextArray[i] + "</option>");
+    }
+  } else {
+    console.log("h4TextArray is not defined or empty.");
+  }
+};
+const handleInputtypeChange = () => {
+  const input3 = $("#selctinputtype").val();
+
+  if (input3 == "Select any input data type") {
+    return;
+  }
+
+  const takeinputdata = $("#takeinputdata");
+  takeinputdata.empty();
+
+  const commonHiddenFields = ["placeholder", "maxlength", "minLength"];
+
+  if (input3 === "select" ) {
+    const numberInput = $("<input>", {
+      type: "number",
+      placeholder: "Enter the number of options",
+      class: "form-control w-50 mt-3",
+      require: true,
+    });
+
+    const buttonSubmit = $("<button>Submit</button>");
+
+    takeinputdata.append(numberInput, buttonSubmit);
+
+    buttonSubmit.click(() => {
+      const noOfOption = parseInt(numberInput.val());
+
+      for (let i = 0; i < noOfOption; i++) {
+        const newInput = $("<input>").attr({
+          placeholder: "Options...",
+          id: 1234,
+        });
+        $("#provideoption").append(newInput);
+      }
+
+      takeinputdata.show();
+    });
+  } 
+ 
+  
+  else if (
+    input3 === "checkbox" || input3 === "radio" || input3 === "date" ||
+    input3 === "file" || input3 === "color" || input3 === "range" ||
+    input3 === "hidden" || input3 === "submit" || input3 === "button"
+  ) {
+    takeinputdata.empty();
+    appendForm(takeinputdata);
+
+    commonHiddenFields.forEach((field) =>
+      $(`#${field}`).hide().prev("label").hide()
+    );
+
+    if (input3 === "submit") {
+      const nameLabel = $("<label>").text("Name");
+      const nameInput = $("<input>").attr({
+        type: "text",
+        id: "inputName",
+      });
+
+      $("#takeinputdata form").prepend(nameLabel, nameInput);
+    }
+  } else {
+    takeinputdata.empty();
+    appendForm(takeinputdata);
+  }
+};
 
 // $(document).ready(function() {
 //     $('#input1').on('input', function() {
